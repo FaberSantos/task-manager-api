fadf62f9bc53dfaa4f11df801f0da11a23d29dce
fabersantos45 <faber@dynamicfactor.com>
2018-02-02 15:26:23 +0800

    Tests for create users action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,4 +12,26 @@ class Api::V1::UsersController < ApplicationController
 
   end
 
+  def create
+    begin
+      @user = User.new(user_params)
+
+      if @user.save
+        render json: @user, status: 201
+      else
+        render json: { errors: @user.errors }, status: 422
+      end
+    rescue
+      head 404
+    end
+
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
