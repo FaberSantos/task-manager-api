9507280c428f979495406c08c3c9382ced17b91d
fabersantos45 <faber@dynamicfactor.com>
2018-02-06 17:53:06 +0800

    add method user_logged_in?

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,11 +1,15 @@
 module Authenticable
 
   def current_user
-    @current_user = User.find_by(auth_token: request.headers['Authorization'])
+    @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
   end
 
   def authenticate_with_token!
-    render json: { errors: 'Unauthorized Access' }, status: 401 unless current_user.present?
+    render json: { errors: 'Unauthorized Access' }, status: 401 unless user_logged_in?
+  end
+
+  def user_logged_in?
+    current_user.present?
   end
 
 end
